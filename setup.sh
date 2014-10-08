11bee890a9e9a2d71bfffb31c53ee490fb014780
UmedaTakefumi <ut+github@azurestone.net>
2014-10-09 01:23:56 +0900

    Add:  設定ファイルをセットアップするスクリプトを追加

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+current_dir=`pwd`
+setuplist=()
+
+while read f; do
+  setuplist+=("$f")
+done < <(find `pwd` -name 'UmedaTakefumi.*' -maxdepth 1 -mindepth 1)
+
+for value in ${setuplist[@]}
+do
+  echo '#--------------------------------------------------------------'
+  echo "# $value / setup now ....."
+  cd $value
+
+  numberline=1
+  cat setupfile.config | while read setupfilename
+  do
+    echo "ln -s $value/${setupfilename} ${HOME}/${setupfilename}"
+    ln -s $value/${setupfilename} ${HOME}/${setupfilename}
+    numberline=$((numberline + 1))
+  done
+
+  cd $current_dir 
+done
 
 
