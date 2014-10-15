f6e49e20456817ec6a98cffbc2f9c723963562ac
UmedaTakefumi <ut+github@azurestone.net>
2014-10-15 09:03:08 +0900

    Fix:  setup.shの修正/unlink機能を追加

--- setup.sh
+++ setup.sh
@@ -3,6 +3,16 @@
 current_dir=`pwd`
 setuplist=()
 
+func_link(){
+  echo "ln -s $value/${setupfilename} ${HOME}/${setupfilename}"
+  ln -s $value/${setupfilename} ${HOME}/${setupfilename}
+}
+
+func_unlink(){
+  echo "unlink ${HOME}/${setupfilename}"
+  unlink ${HOME}/${setupfilename}
+}
+
 while read f; do
   setuplist+=("$f")
 done < <(find `pwd` -name 'UmedaTakefumi.*' -maxdepth 1 -mindepth 1)
@@ -16,8 +26,13 @@ do
   numberline=1
   cat setupfile.config | while read setupfilename
   do
-    echo "ln -s $value/${setupfilename} ${HOME}/${setupfilename}"
-    ln -s $value/${setupfilename} ${HOME}/${setupfilename}
+    if [ -e "${HOME}/${setupfilename}" ]; then
+      func_unlink
+      func_link
+    else
+      func_link
+    fi
+      
     numberline=$((numberline + 1))
   done
 
