b11f371b0054a195e713708717383d75201dbe20
UmedaTakefumi <ut+github@azurestone.net>
2014-10-30 02:53:30 +0900

    Fix:  セットアップスクリプトの修正/不要なコードを削除
      * 使用していないコメントアウトしていたコードを削除

--- setup.sh
+++ setup.sh
@@ -12,31 +12,19 @@ func_unlink(){
   unlink ${HOME}/${setupfilename}
 }
 
-#func_git_submodule_setup
+echo '#--------------------------------------------------------------'
+echo "# setup now ....."
 
-#while read f; do
-#  setuplist+=("$f")
-#done < <(find `pwd` -name 'UmedaTakefumi.*' -maxdepth 1 -mindepth 1)
-
-#for value in ${setuplist[@]}
-#do
-  echo '#--------------------------------------------------------------'
-  echo "# setup now ....."
-#  cd $value
-
-  numberline=1
-  cat setupfiles | while read setupfilename
-  do
-    if [ -e "${HOME}/${setupfilename}" ]; then
-      func_unlink
-      func_link
-    else
-      func_link
-    fi
-    numberline=$((numberline + 1))
-  done
-
-#  cd $current_dir 
-#done
+numberline=1
+cat setupfiles | while read setupfilename
+do
+  if [ -e "${HOME}/${setupfilename}" ]; then
+    func_unlink
+    func_link
+  else
+    func_link
+  fi
+  numberline=$((numberline + 1))
+done
 
 
