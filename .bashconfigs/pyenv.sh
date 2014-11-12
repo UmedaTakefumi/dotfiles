8b5ce60c43797d8fe605125161f413bba95b4160
UmedaTakefumi <ut+github@azurestone.net>
2014-11-12 16:47:11 +0900

    Add: pyenvの設定を追加

---
+++ .bashconfigs/pyenv.sh
@@ -0,0 +1,13 @@
+script_name=pyenv.sh
+echo "Loding $script_name ..."
+
+# plenv'
+
+export PYENV_ROOT="$HOME/Software/pyenv"
+export PATH="$HOME/Software/pyenv/bin:$PATH"
+
+eval "$(pyenv init -)"
+
+
+
+
