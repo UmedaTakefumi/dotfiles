87043b69a4b7c14f8cdf9e56c4e9aa69308a2876
UmedaTakefumi <ut+github@azurestone.net>
2015-07-14 11:38:45 +0900

    Fix:  bashconfig の修正
      * bash_completion がローディングされない問題を解決

--- .bashconfigs/bash_completion.sh
+++ .bashconfigs/bash_completion.sh
@@ -1,8 +1,12 @@
 script_name=bash_completion.sh
 echo "Loding $script_name ..."
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
+#if [ -f $(brew --prefix)/etc/bash_completion ]; then
+#  . $(brew --prefix)/etc/bash_completion
+#fi
+
+source /usr/local/Cellar/bash-completion/1.3/etc/profile.d/bash_completion.sh
+
+
 
 
