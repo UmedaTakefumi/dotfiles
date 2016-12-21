a9f8341e125a18da4180211b099502e1cfc0b35d
UmedaTakefumi <ut+github@azurestone.net>
2016-12-21 15:44:01 +0900

    使いにくい cd funcを削除

--- .bashconfigs/prompt.sh
+++ .bashconfigs/prompt.sh
@@ -4,12 +4,12 @@ RAND_CHAR=`cat /dev/urandom | LC_ALL=C  tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n
 L1='\[\e[$[COLUMNS-$(echo -n " [\u@\h]" | wc -c)]C\e[m[\u@\h]\e[\e[$[COLUMNS]D\]'
 PS1="[\w]\n$RAND_CHAR:$ "
 
-function cd () {
-  clear
-  builtin cd $1
-  echo [`pwd`]
-  ll
-}
+#function cd () {
+#  clear
+#  builtin cd $1
+#  echo [`pwd`]
+#  ll
+#}
 
 #PS1="$RAND_CHAR:$ "
 
