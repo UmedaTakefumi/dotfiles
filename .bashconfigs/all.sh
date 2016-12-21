20a6800d104c6a65198a77fb1e6e46334f75ab43
UmedaTakefumi <ut+github@azurestone.net>
2016-12-21 15:47:42 +0900

    bash completion機能がmacosxに依存していたため、一旦機能を無効化

--- .bashconfigs/all.sh
+++ .bashconfigs/all.sh
@@ -3,7 +3,7 @@ source $HOME/.bashconfigs/cmd_aliases.sh
 source $HOME/.bashconfigs/ls-color.sh
 #source $HOME/.bashconfigs/packer.sh
 #source $HOME/.bashconfigs/rbenv.sh
-source $HOME/.bashconfigs/bash_completion.sh
+#source $HOME/.bashconfigs/bash_completion.sh
 #source $HOME/.bashconfigs/golang.sh
 #source $HOME/.bashconfigs/plenv.sh
 #source $HOME/.bashconfigs/pyenv.sh
