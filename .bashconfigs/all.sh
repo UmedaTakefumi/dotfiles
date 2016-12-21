7ea8964b15aa5dc46a9e2eeed9b8bec5509b641a
UmedaTakefumi <ut+github@azurestone.net>
2016-12-21 15:46:13 +0900

    不要な機能を削除: **env系

--- .bashconfigs/all.sh
+++ .bashconfigs/all.sh
@@ -1,12 +1,12 @@
 source $HOME/.bashconfigs/history.sh
 source $HOME/.bashconfigs/cmd_aliases.sh
 source $HOME/.bashconfigs/ls-color.sh
-source $HOME/.bashconfigs/packer.sh
-source $HOME/.bashconfigs/rbenv.sh
+#source $HOME/.bashconfigs/packer.sh
+#source $HOME/.bashconfigs/rbenv.sh
 source $HOME/.bashconfigs/bash_completion.sh
-source $HOME/.bashconfigs/golang.sh
-source $HOME/.bashconfigs/plenv.sh
-source $HOME/.bashconfigs/pyenv.sh
+#source $HOME/.bashconfigs/golang.sh
+#source $HOME/.bashconfigs/plenv.sh
+#source $HOME/.bashconfigs/pyenv.sh
 source $HOME/.bashconfigs/prompt.sh
 #source $HOME/.bashconfigs/anyenv.sh
 
