ccafae05e0ee10c2a5510c9fdf1dc0461eb06cab
UmedaTakefumi <ut+github@azurestone.net>
2017-04-30 19:15:04 +0900

    fuga

--- .bashconfigs/golang.sh
+++ .bashconfigs/golang.sh
@@ -1,11 +1,10 @@
 script_name=golang.sh
 echo "Loding $script_name ..."
 
-
-if [ -e ${HOME}/Data/Develop-AZ/Library/gocode ]; then
+if [ ! -d ${HOME}/Data/Develop-AZ/Library/gocode ]; then
   
-else
   mkdir -p ${HOME}/Data/Develop-AZ/Library/gocode
+
 fi
 
 export GOPATH=$HOME/Data/Develop-AZ/Library/gocode
