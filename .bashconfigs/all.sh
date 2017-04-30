808b21c21a548d4b638aae3bf98b237b98c0b95c
UmedaTakefumi <ut+github@azurestone.net>
2017-04-30 18:51:56 +0900

    goの設定を有効

--- .bashconfigs/all.sh
+++ .bashconfigs/all.sh
@@ -4,7 +4,7 @@ source $HOME/.bashconfigs/ls-color.sh
 #source $HOME/.bashconfigs/packer.sh
 #source $HOME/.bashconfigs/rbenv.sh
 #source $HOME/.bashconfigs/bash_completion.sh
-#source $HOME/.bashconfigs/golang.sh
+source $HOME/.bashconfigs/golang.sh
 #source $HOME/.bashconfigs/plenv.sh
 #source $HOME/.bashconfigs/pyenv.sh
 source $HOME/.bashconfigs/prompt.sh
