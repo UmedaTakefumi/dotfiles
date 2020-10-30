03bef55c64ba3548e1ca6b0f600c3a066fead7cc
UmedaTakefumi <ut+github@azurestone.net>
2020-10-30 20:38:19 +0900

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-get_resource_url="https://github.com/UmedaTakefumi/dotfiles/archive/master.zip"
+echo "00. install brew"
+## ref. https://brew.sh/index_ja
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo $get_resource_url
+echo "01. install app-list/brew"
+## export:
+##   $ brew leaves > brew.txt
+## 
+## ref. https://qiita.com/qoAop/items/7b7533c716ba1fa4c16b
+## 
+xargs brew install < brew.txt 
+
+echo "02. install app-list/brew-cask"
+## export: 
+## 		$ brew cask list -1 > brew_cask.txt
+##
+## ref. https://qiita.com/qoAop/items/7b7533c716ba1fa4c16b
+## 
+xargs brew cask install < brew_cask.txt
+
+
