9192ff2e8eadc255808adddd9186af28d7e6df3a
UmedaTakefumi <ut+github@azurestone.net>
2020-11-04 01:10:23 +0900

    edit zsh-completion-conf

--- .zshconfigs/zsh_completions_drawin.sh
+++ .zshconfigs/zsh_completions_drawin.sh
@@ -1,33 +1,36 @@
-# $ brew install zsh-completions
-# 
-# -- snip --
-#
-# ==> Caveats
-# To activate these completions, add the following to your .zshrc:
-# 
-#   if type brew &>/dev/null; then
-#     FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-# 
-#     autoload -Uz compinit
-#     compinit
-#   fi
-# 
-# You may also need to force rebuild `zcompdump`:
-# 
-#   rm -f ~/.zcompdump; compinit
-# 
-# Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
-# to load these completions, you may need to run this:
-# 
-#   chmod go-w '/usr/local/share'
-# ==> Summary
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+if [ $SHELL = "/bin/zsh" ]; then
 
-  autoload -Uz compinit
-  compinit
-fi
+  # $ brew install zsh-completions
+  # 
+  # -- snip --
+  #
+  # ==> Caveats
+  # To activate these completions, add the following to your .zshrc:
+  # 
+  #   if type brew &>/dev/null; then
+  #     FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  # 
+  #     autoload -Uz compinit
+  #     compinit
+  #   fi
+  # 
+  # You may also need to force rebuild `zcompdump`:
+  # 
+  #   rm -f ~/.zcompdump; compinit
+  # 
+  # Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
+  # to load these completions, you may need to run this:
+  # 
+  #   chmod go-w '/usr/local/share'
+  # ==> Summary
+  if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  
+    autoload -Uz compinit
+    compinit
+  fi
 
+else 
 
-
+  echo "zsh..?"
 
