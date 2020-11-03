b05bc0bd9c7bfbb5b190b74f570653cb111fffe9
UmedaTakefumi <ut+github@azurestone.net>
2020-11-04 02:56:03 +0900

    edit build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,6 @@ echo "01. install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "02. install app-list/brew"
-brew_export()  {
-  brew leaves > brew.txt
-  brew cask list -1 > brew_cask.txt
-  ## ref. https://qiita.com/qoAop/items/7b7533c716ba1fa4c16b
-}
 xargs brew install < brew.txt 
 
 echo "03. install app-list/brew-cask"
