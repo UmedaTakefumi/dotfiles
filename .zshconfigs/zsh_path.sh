54074bf13ce6776fbd599f20b2085e2f7d64e355
UmedaTakefumi <ut+github@azurestone.net>
2020-11-08 20:13:06 +0900

    edit zsh-conf

--- .zshconfigs/zsh_path.sh
+++ .zshconfigs/zsh_path.sh
@@ -4,3 +4,16 @@
 #For compilers to find libiconv you may need to set:
 #  export LDFLAGS="-L/usr/local/opt/libiconv/lib"
 #  export CPPFLAGS="-I/usr/local/opt/libiconv/include"
+
+
+If you need to have sqlite first in your PATH run:
+  echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"' >> ~/.zshrc
+
+For compilers to find sqlite you may need to set:
+  export LDFLAGS="-L/usr/local/opt/sqlite/lib"
+  export CPPFLAGS="-I/usr/local/opt/sqlite/include"
+
+For pkg-config to find sqlite you may need to set:
+  export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
+
+azurestone@ZGMF-X10A dotfiles %
