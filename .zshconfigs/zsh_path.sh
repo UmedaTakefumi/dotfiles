6d663c118b8f4822ae80ba0a90dced096a676f54
UmedaTakefumi <ut+github@azurestone.net>
2020-11-12 23:03:07 +0900

    add gradle

--- .zshconfigs/zsh_path.sh
+++ .zshconfigs/zsh_path.sh
@@ -1,19 +1,6 @@
-#If you need to have libiconv first in your PATH run:
-#  echo 'export PATH="/usr/local/opt/libiconv/bin:$PATH"' >> ~/.zshrc
-#
-#For compilers to find libiconv you may need to set:
-#  export LDFLAGS="-L/usr/local/opt/libiconv/lib"
-#  export CPPFLAGS="-I/usr/local/opt/libiconv/include"
-
-
-If you need to have sqlite first in your PATH run:
-  echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"' >> ~/.zshrc
-
-For compilers to find sqlite you may need to set:
-  export LDFLAGS="-L/usr/local/opt/sqlite/lib"
-  export CPPFLAGS="-I/usr/local/opt/sqlite/include"
-
-For pkg-config to find sqlite you may need to set:
-  export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
-
-azurestone@ZGMF-X10A dotfiles %
+export LDFLAGS="-L/usr/local/opt/libiconv/lib"
+export CPPFLAGS="-I/usr/local/opt/libiconv/include"
+export LDFLAGS="-L/usr/local/opt/sqlite/lib"
+export CPPFLAGS="-I/usr/local/opt/sqlite/include"
+export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
+export CPPFLAGS="-I/usr/local/opt/openjdk/include"
