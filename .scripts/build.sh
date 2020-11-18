192a9af611652a8b9360b7111bc764e2551308c2
UmedaTakefumi <ut+github@azurestone.net>
2020-11-18 21:04:28 +0900

    edit build.sh

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source ./build.functions
+
 echo "00. init dir and download archive"
 mkdir -p ~/Data/repos
 mkdir -p ~/{tmp,Dockers,Develop-AZ,GoogleDrive,OneDrive,Vagrant,Logs}
@@ -11,10 +13,10 @@ echo "01. install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "02. install app-list/brew"
-xargs brew install < brew.txt 
+xargs brew install < ../.configs/brew.txt 
 
 echo "03. install app-list/brew-cask"
-xargs brew cask install < brew_cask.txt
+xargs brew cask install < ../configs/brew_cask.txt
 
 ## ref. https://qiita.com/UmedaTakefumi/items/c81e596b246e48aa2c16
 pyenv install -l | grep -E "\s3.[0-9.]+$" | sort -nr | head -2 | xargs -I{} pyenv install {}
