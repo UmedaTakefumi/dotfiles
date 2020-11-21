20fc16342ac8842a12e1a5efe7522fed435a0856
UmedaTakefumi <ut+github@azurestone.net>
2020-11-22 00:03:22 +0900

    add-bashconfig

---
+++ .bashconfigs/bash_alias.sh
@@ -0,0 +1,45 @@
+DOTFILES_STANDARD_PATH=$HOME/Data/repos/github.com/UmedaTakefumi/dotfiles
+
+#a  () { }
+#b  () {  }
+c  ()	{ clear }
+#d  () {  }
+#e  () {  }
+#f  () {  }
+g  ()   { cd $(ghq root)/$(ghq list) | peco }
+gc ()   { git checkout $* }
+gh ()   { ghq get $* }
+gom ()  { git push origin master $* }
+gp ()   { git pull $* }
+gs ()	{ git status }
+h  ()   { history }
+#i  () {  }
+#j  () {  }
+#k  () {  }
+l  ()   { ls -lasF $* }
+#m  () {  }
+#n  () {  }
+#o  () {  }
+p  ()   { python }
+#q  () {  }
+#r  () {  }
+#s  () {  }
+#t  () {  }
+#u  () {  }
+v  ()   {
+  if [ -e $HOME/.vimconfigs/all.vim ]; then
+    vim -u $HOME/.vimconfigs/all.vim $*
+
+  elif [ -e $DOTFILES_STANDARD_PATH/.vimconfigs/all.vim ]; then
+    vim -u $DOTFILES_STANDARD_PATH/.vimconfigs/all.vim $*
+
+  else
+    vim $*
+
+  fi
+
+}
+#w  () {  }
+#x  () {  }
+#y  () {  }
+#z  () {  }
