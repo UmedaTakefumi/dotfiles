8bde6f6ef4f1deddea3748f1eeec815a57d22351
UmedaTakefumi <ut+github@azurestone.net>
2020-12-07 01:16:37 +0900

    edit script

--- .scripts/pktdump.sh
+++ .scripts/pktdump.sh
@@ -5,5 +5,15 @@ SAVE_DIRPATH="/opt/netdump/"
 LOTATION_SEC="60"
 CAP_INTERFACE="enp0s8"
 
-tshark -b  duration:${LOTATION_SEC} -s 0 -i ${CAP_INTERFACE} -w ${}/netdump_${DATE_TIME}.pcap
+
+if type "/usr/sbin/tshark" > /dev/null 2>&1; then
+
+  tshark -b duration:${LOTATION_SEC} -s 0 -i ${CAP_INTERFACE} -w ${SAVE_DIRPATH}/netdump_${DATE_TIME}.pcap
+else
+  echo "Please install wireshark."
+fi
+
+
+
+
 
