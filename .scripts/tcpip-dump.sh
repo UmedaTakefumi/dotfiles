1e1459de9414a66d79264f63c2b705e8b31c96ee
UmedaTakefumi <ut+github@azurestone.net>
2020-12-07 01:22:31 +0900

    edit script

--- .scripts/tcpip-dump.sh
+++ .scripts/tcpip-dump.sh
@@ -8,7 +8,7 @@ CAP_INTERFACE="enp0s8"
 
 if type "/usr/sbin/tshark" > /dev/null 2>&1; then
 
-  tshark -b duration:${LOTATION_SEC} -s 0 -i ${CAP_INTERFACE} -w ${SAVE_DIRPATH}/netdump_${DATE_TIME}.pcap
+  tshark -b duration:${LOTATION_SEC} -s 0 -i ${CAP_INTERFACE} -w ${SAVE_DIRPATH}/tcpip-dump_${DATE_TIME}.pcap
 else
   echo "Please install wireshark."
 fi
