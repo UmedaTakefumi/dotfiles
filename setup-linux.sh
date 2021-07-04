10179481008fd46291f99e0239fd9a9149e9fb27
UmedaTakefumi <ut+github@azurestone.net>
2021-07-04 21:36:57 +0900

    edit linux-setup-scripts

--- setup-linux.sh
+++ setup-linux.sh
@@ -5,11 +5,11 @@ sudo yum -y install chromium firefox vim screen
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 yum check-update
-sudo yum install code
+sudo yum -y install code
 sudo yum -y install kubernetes podman
 LANG=C xdg-user-dirs-update
 sudo yum -y install wireshark
 sudo yum -y install libreoffice
-
+sudo yum -y install git
 
 
