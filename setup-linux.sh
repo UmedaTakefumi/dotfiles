ff4c89ab620a71b66b4cd2e24e512dfc9d3aed99
UmedaTakefumi <ut+github@azurestone.net>
2021-07-14 23:42:18 +0900

    edit linux-setup-scripts

--- setup-linux.sh
+++ setup-linux.sh
@@ -6,7 +6,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 yum check-update
 sudo yum -y install code
-sudo yum -y install kubernetes podman
 LANG=C xdg-user-dirs-update --force
 sudo yum -y install wireshark
 sudo yum -y install libreoffice
